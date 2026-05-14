3267bfad79018b5b7246479fde7a46cf98e5293c
raphaellim <138130040+raphaellim@users.noreply.github.com>
2026-05-14 19:00:26 +0900

    Update server.js

--- server.js
+++ server.js
@@ -137,6 +137,8 @@ io.on("connection", (socket) => {
   });
 });
 
-server.listen(3000, () => {
-  console.log("경매 서버 실행: http://localhost:3000/auction.html");
+const PORT = process.env.PORT || 3000;
+
+server.listen(PORT, () => {
+  console.log(`경매 서버 실행 : ${PORT}`);
 });
